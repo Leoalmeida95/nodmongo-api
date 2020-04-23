899880a44d53c4b034f5e48a552ae1cc1048fe08
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-23 10:58:31 -0300

    ajuste network docker / criando autenticacao

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
-
-
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async(req,res)=>{
     try{
@@ -16,10 +15,29 @@ router.post('/register', async(req,res)=>{
 
         user.password = undefined; //não retorna a senha no json
 
-        return res.send({user});
+        return res.status(201).send({user});
 
     }catch(err){
-        return res.status(400).send({error:'Registrantion failure.'});
+        return res.status(500).send({error:'Registrantion failure.'});
+    }
+});
+
+router.post('/authenticate', async(req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email}).select('+password'); //busca o usuário e explicita a necessidade do retorno da senha (select false)
+
+        if(!user)
+            return res.status(400).send({error:'Uset not found.'});
+
+        if(!await bcrypt.compare(password, user.password))
+            return res.status(400).send({error:'Invalid password'});
+
+        return res.status(200).send({user});
+
+    }catch(err){
+        return res.status(500).send({error:'Authentication failure.'});
     }
 });
 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req,res)=>{
-  res.send('v8');
+  res.send('User Auth');
 })
 
 const auth = require('./controllers/authController');
